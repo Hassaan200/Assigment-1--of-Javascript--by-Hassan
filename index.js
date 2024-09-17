a609b9f67612d006ce83e60801f2b7c666975f34
Hassaan200 <hassaankhaliq45@gmail.com>
2024-09-17 17:24:12 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,248 @@
+// chapter 1
+
+// alert("Error! Please enter valid password.");
+
+
+// alert("Welcome to JS land...\nHappy coding!");
+
+// alert("Welcome to JS land...");
+
+// alert("Happy Coding!.");
+// alert("Hello... I can run Javascript through my web browser's comsole");
+
+
+
+// Chapter 2
+
+
+// var username = ""
+// var myname = "Hassan"
+
+// console.log(myname)
+
+// var message = "Hello World";
+
+// alert(message);
+
+// console.log(message)
+
+
+
+// var name1 = "John cena"
+// alert(name1)
+// var name2 = "20 years old"
+// alert(name2)
+// var name3 = "Certified Mobile Application Development"
+// alert(name3)
+
+
+// var burger = "Pizza"
+
+// alert(`${burger}\n${burger.slice(0, 4)}\n${burger.slice(0, 3)}\n${burger.slice(0, 2)}\n${burger.slice(0, 1)}`);
+
+
+// var email = "hassaankhaliq45@gmail.com"
+// console.log(email);
+// alert("My email address is " + email);
+
+
+// var book =  "A smarter way to learn JavaScript"
+// console.log (book);
+// alert("I am trying to learn from the book "+ book);
+
+// document.write(" Yah! I can write HTML content through Javascript ");
+
+
+// var value = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// console.log(value)
+// alert(value)
+
+
+// chapter 3  
+
+
+// var age = 21
+// console.log (age);
+// alert("I am "  + age + " years old ");
+
+
+
+// var visit = 0;
+// visit = 1;
+// alert("You have visited this site " +  visit + " times ");
+
+
+// var birthyear = 2003;
+// console.log (birthyear);
+// document.write("My birth year is " + birthyear + " <br> Data type of my declared variable number");
+
+
+// var Visitor_name = "John Doe "
+// var quantity = "ordered 5 T-shirt(s) "
+// var Product_title = "on XYZ Clothing store."
+// document.write(Visitor_name + quantity + Product_title);
+
+
+// chapter 4
+
+var a = 10, b = 20, c = 30;
+console.log(a + b + c);
+
+// legal variables
+
+let myVariable;
+let _name;
+let $price;
+let firstName;
+let age2;
+
+// Illegal variables
+
+// let 2name;      
+// let first-name; 
+// let my name;    
+// let var;        
+// let #score;  
+
+
+
+// chapter 5
+
+
+// var num1 = 3
+// var  num2 = 5
+// var sum = num1 + num2
+// console.log("Sum of 3 and 5 is " + sum)
+// document.write("Sum of 3 and 5 is " + sum);
+
+// var sub = num1 -  num2;
+// console.log("Subtraction of 3 and 5 is " + sub  ); 
+// document.write(" Subtraction of 3 and 5 is " + sub);
+
+// var mult = num1 *  num2;
+// console.log("Multiplication of 3 and 5 is " + mult  ); 
+// document.write(" Multiplication of 3 and 5 is " + mult);
+
+// var divide = num1 /  num2;
+// console.log("Division of 3 and 5 is " + divide  ); 
+// document.write(" Division of 3 and 5 is " + divide);
+
+
+// question 3
+
+// var No = ""
+// document.write("Value after variable declaration is " + No);
+// console.log("Value after variable declaration is " + No);
+// No = 5;
+// document.write("<br> Value after variable initializing is " + No);
+// console.log("Value after variable  initializing is " + No++);
+// document.write("<br> Value after increment is " + No);
+// console.log("Value after increment variable is " + No);
+
+// var add = No + 7 ;
+// document.write("<br> Value after addition is " + add );
+// console.log("Value after addition variable is " + add-- );
+
+// document.write("<br> Value after decrement is " + add );
+// console.log("Value after decrement variable is " + add );
+
+// document.write("<br> The remainder is : " + add % 3 + " after division of 12 from 3");
+// console.log("The remainder is : " + add % 3 + " after division of 12 from 3 ");
+
+// Question 4
+
+// var ticket = 600
+// document.write("Total cost to buy 5 Tickets to a movie is " + 600 * 5 + "PKR")
+
+// Question 5
+
+// var table = 4;
+// document.write("Table of " + table);
+// document.write("<br> 4x1 = " + table * 1);
+// console.log("4x1 =" + table * 1);
+// document.write("<br> 4x2 = " + table * 2);
+// console.log("4x2 =" + table * 2);
+// document.write("<br> 4x3 = " + table * 3);
+// console.log("4x3 =" + table * 3);
+// document.write("<br> 4x4 = " + table * 4);
+// console.log("4x4 =" + table * 4);
+// document.write("<br> 4x5 = " + table * 5);
+// console.log("4x5 =" + table * 5);
+// document.write("<br> 4x6 = " + table * 6);
+// console.log("4x6 =" + table * 6);
+// document.write("<br> 4x7 = " + table * 7);
+// console.log("4x7 =" + table * 7);
+// document.write("<br> 4x8 = " + table * 8);
+// console.log("4x8 =" + table * 8);
+// document.write("<br> 4x9 = " + table * 9);
+// console.log("4x9 =" + table * 9);
+// document.write("<br> 4x10 = " + table * 10);
+// console.log("4x10 =" + table * 10);
+
+
+//   Question 6
+
+// var C = 25;
+// var  F = ((C * 9/5) + 32);
+// document.write(C +"°C is " + F + "°F");
+
+// var F = 70;
+// var C = ((F - 32) * 5/9);
+
+// document.write("<br>" + F +"°F is " + C + "°C");
+
+// Question 7
+
+// var p1 = 650;
+// var p2 = 100;
+// var q1 = 3;
+// var q2 = 7;
+// var shipcharges = 100;
+
+// document.write("Price of item 1 is " + p1)
+// document.write("<br> Quantity of item 1 is " + q1)
+// document.write("<br> Price of item 2 is " + p2)
+// document.write("<br> Quantity of item 2 is " + q2)
+// document.write("<br> Shipping charges is " + shipcharges)
+// document.write("<br> Total cost of your order is  " + ((p1 * 3) + ( p2 * 7 )+ shipcharges) );
+
+// Question 8
+
+// var tm = 980 ;
+// var marks = 804 ;
+// var percentage = (marks / tm) * 100 ;
+// document.write("Total Marks : " + tm);
+// document.write("<br> Marks Obtained : " + marks);
+// document.write("<br> Percentage : " + percentage + "%");
+
+// Qustion 9
+
+var $ = 10
+var riyals = 25 ;
+var rupee$ = 104.80 ;
+var rupees_riyal = 2
+document.write("Total currency in PKR: " +  (rupees_riyal * riyals) + (rupee$ *  $) );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
